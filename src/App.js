2e42227e6cbba85eb45f33b2e419a37e9d5a3400
Luc Nikiema <ln358@georgetown.edu>
2021-02-23 22:26:06 -0500

    Deployment Take 8

--- src/App.js
+++ src/App.js
@@ -194,7 +194,7 @@ const App = () => {
   }
 
   function isSignedIn(){
-    fetch("https://seamless-io.herokuapp.com/isSignedIn", {
+    fetch("https://seamless-io.herokuapp.com:5000/isSignedIn", {
       method: "POST",
       mode: "cors",
       headers: {
@@ -247,7 +247,7 @@ const App = () => {
   }
 
   function makePlaylist(search_str) {
-    fetch("https://seamless-io.herokuapp.com/playlist", {
+    fetch("https://seamless-io.herokuapp.com:5000/playlist", {
       method: "POST",
       mode: "cors",
       headers: {
@@ -285,7 +285,7 @@ const App = () => {
   }
 
   function postPlaylist(ans) {
-    fetch("https://seamless-io.herokuapp.com/postPlaylist", {
+    fetch("https://seamless-io.herokuapp.com:5000/postPlaylist", {
       method: "POST",
       mode: "cors",
       headers: {
@@ -315,7 +315,7 @@ const App = () => {
   }
 
   function signOut () {
-    fetch("https://seamless-io.herokuapp.com/signOut", {
+    fetch("https://seamless-io.herokuapp.com:5000/signOut", {
       method: "POST",
       mode: "cors",
       headers: {
